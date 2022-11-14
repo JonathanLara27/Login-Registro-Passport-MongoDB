965f83d316eb671753a8aaeaa3d11e8e91c387a1
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2022-11-14 16:40:28 -0500

    subiendo proyecto

---
+++ src/passport/local-auth.js
@@ -0,0 +1,49 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+passport.use('local-signup', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, email, password, done) => {
+  const user = await User.findOne({'email': email})
+  console.log(user)
+  if(user) {
+    return done(null, false, req.flash('signupMessage', 'Este correo ya se encuentra registrado.'));
+  } else {
+    const newUser = new User();
+    newUser.email = email;
+    newUser.password = newUser.encryptPassword(password);
+    newUser.name = req.body.name;
+    newUser.lastname = req.body.lastname;
+  console.log(newUser)
+    await newUser.save();
+    done(null, newUser);
+  }
+}));
+
+passport.use('local-signin', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, email, password, done) => {
+  const user = await User.findOne({email: email});
+  if(!user) {
+    return done(null, false, req.flash('signinMessage', 'Usuario no registrado'));
+  }
+  if(!user.comparePassword(password)) {
+    return done(null, false, req.flash('signinMessage', 'Contraseña incorrecta'));
+  }
+  return done(null, user);
+}));
